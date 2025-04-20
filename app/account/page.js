37151f630ceb07d8ada8b060336694b73ca0c0fe
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-04-20 19:46:27 +0200

    Adding Nested Routes and Pages

--- app/account/page.js
+++ app/account/page.js
@@ -1,13 +1,13 @@
 
 
 export const metadata = {
-    title: 'Account',
+    title: 'Guest area',
 }
 
 export default function Page() {
     return (
-        <div>
-            <h1>Account Page</h1>
-        </div>
+        <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+            Welcome, Jonas
+        </h2>
     );
 }
