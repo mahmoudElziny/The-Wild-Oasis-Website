b5c7ec27eadb1f9a5bf3acd77b6d53dc91b69432
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-05-11 05:38:46 +0300

    Getting the User Session

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -15,12 +17,22 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
+          {session?.user?.image ? (
+            <Link
+            href="/account"
+            className="hover:text-accent-400 transition-colors flex items-center gap-4"
+          >
+            <img src={session.user.image} className="w-8 h-8 rounded-full" alt={session.user.name} referrerPolicy="no-referrer"/>
+            <span>{session.user.name}</span>
+          </Link>
+          ): (
+            <Link
             href="/account"
             className="hover:text-accent-400 transition-colors"
           >
             Guest area
           </Link>
+          ) }
         </li>
       </ul>
     </nav>
